56018f339993572d24548948c61e8366af6dd26a
David Benkner <d.benkner@gmail.com>
2023-08-31 10:26:14 -0400

    Add project files.

---
+++ PRS/Data/PRSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PRS.Models;
+
+namespace PRS.Data
+{
+    public class PRSContext : DbContext
+    {
+        public PRSContext (DbContextOptions<PRSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PRS.Models.User> Users { get; set; } = default!;
+    }
+}
