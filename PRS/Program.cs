2cecec41f230c1fdb9ce6a1575ff53c1701d95d5
David Benkner <d.benkner@gmail.com>
2023-11-03 08:49:37 -0400

    app published

--- PRS/Program.cs
+++ PRS/Program.cs
@@ -2,12 +2,18 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PRS.Data;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<PRSContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DevDb") ?? throw new InvalidOperationException("Connection string 'DevDb' not found.")));
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var connStrKey = "ProdDb";
+#if DEBUG
+    connStrKey="DevDb";
+#endif
+builder.Services.AddDbContext<PRSContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString(connStrKey) ?? throw new InvalidOperationException("Connection string 'DevDb' not found.")));
+
 builder.Services.AddCors();
 
 var app = builder.Build();
@@ -15,7 +21,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); //Will be changed when running on the server
 
 app.UseAuthorization();
 
